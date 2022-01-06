99e65d99cfc4f7e055b076fa124cc64c9e7acec3
Mayahi Adel <adel.mayahii@gmail.com>
2022-01-06 23:53:10 +0100

    deploy config

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.use("/api/post", postRoutes);
 if (process.env.NODE_ENV === "production") {
   //set static folder
   app.use(express.static(path.join(__dirname, "../build")));
-  app.get("*", (req, res) => {
+  app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../build"));
   });
 }
